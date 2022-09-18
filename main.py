14cfaf5e634cfc58e880e57408be91d6d675c170
CarmenIM <marginean.carmen.ioana@gmail.com>
2022-09-18 19:02:27 +0300

    HM

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#Homework number 1
+
 my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
 
 # print the initial list
@@ -26,5 +28,6 @@ for i in my_list:
     if i % 3 != 0:
         continue
     print(f'can be divided by 3 -> {i}')
+
 
 
