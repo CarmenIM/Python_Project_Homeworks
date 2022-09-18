328108acf014ce48ef790789a393cd6439e0070e
CarmenIM <marginean.carmen.ioana@gmail.com>
2022-09-18 22:27:08 +0300

    HM2

--- HM2.py
+++ HM2.py
@@ -1,26 +1,51 @@
-def get_sum(n):
-    if n == 0:
-        return 0
-    return n + get_sum(n-1)
+# 1 First
+def my_function(*args, **kwargs):
+    sum = 0
 
-# Example where n=10
-print(get_sum(10))
-
-number_list=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-
-def calculate_odd_and_even():
-    odd_number = 0
-    even_number = 0
-    for i in number_list:
-        if i % 2 == 0:
-            even_number = even_number + i
-        else:
-            odd_number = odd_number + i
-
-    return (odd_number, even_number)
-
-my_tuple = calculate_odd_and_even()
-print(my_tuple)
+    for x in args:
+        if type(x) == int or type(x) == float:
+            sum =sum + x
+    else:
+        return sum
 
 
+print(my_function(1, 5, -3, 'abc', [12, 56, 'cad']))
+print(my_function())
+print(my_function(2, 4, 'abc', param_1=2))
 
+
+# 2 Sum of even numbers selected from a value
+maximum = int(input(" Please Enter the Maximum Value : "))
+total = 0
+
+for number in range(1, maximum+1):
+    if(number % 2 == 0):
+        print("{0}".format(number))
+        total = total + number
+
+print("The Sum of Even Numbers from 1 to {0} = {1}".format(number, total))
+
+# Sum of odd numbers selected from a value
+
+maximum = int(input(" Please Enter the Maximum Value : "))
+Oddtotal = 0
+
+for number in range(1, maximum+1):
+    if(number % 2 != 0):
+        print("{0}".format(number))
+        Oddtotal = Oddtotal + number
+
+print("The Sum of Odd Numbers from 1 to {0} = {1}".format(number, Oddtotal))
+
+
+# Return the value inserted if the number is integer
+
+user_input = input("Please enter the number here -> ")
+
+try:
+    int(user_input)
+    it_is = user_input
+except ValueError:
+    it_is = 0
+
+print(it_is)
