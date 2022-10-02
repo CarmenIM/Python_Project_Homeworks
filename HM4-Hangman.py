83a79ca2a11984177a7e9f1dcafd63036647f76f
CarmenIM <marginean.carmen.ioana@gmail.com>
2022-10-03 00:52:23 +0300

    HM_4_Hangman

---
+++ HM4-Hangman.py
@@ -0,0 +1,34 @@
+import random
+
+with open("Words.txt", "r") as f:
+    words = f.readlines()
+
+word = random.choice(words)[:-1]
+allowed_errors = 3
+guesses = []
+done = False
+
+while not done:
+    for letter in word:
+        if letter.lower() in guesses:
+            print(letter, end=" ")
+        else:
+            print("_", end=" ")
+    print("")
+
+    guess = input(f"Allowed errors left {allowed_errors}, Next guess please: ")
+    guesses.append(guess.lower())
+    if guess.lower() not in word.lower():
+        allowed_errors -= 1
+        if allowed_errors == 0:
+             break
+
+        done = True
+        for letter in word:
+            if letter.lower() not in guesses:
+                done = False
+
+if done:
+    print(f"You won! The word was {word}")
+else:
+    print(f"You lost. The word was {word}")
